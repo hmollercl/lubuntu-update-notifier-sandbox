2af52a099ad272b5710ff4422e5b4b5e6d02d400
Hans P. Möller <hmoller@gmail.com>
2019-01-09 23:23:47 -0300

    working

--- upgrader.py
+++ upgrader.py
@@ -6,7 +6,7 @@
 import sys
 import apt
 import apt_pkg
-from PyQt5.QtWidgets import (QWidget, QApplication)
+from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QProgressBar, QTreeView)
 from PyQt5 import uic
 from PyQt5.QtCore import (Qt, QProcess)
 from PyQt5.QtGui import (QStandardItemModel, QIcon)
@@ -21,19 +21,42 @@ from aptdaemon.errors import NotAuthorizedError, TransactionFailed
 from pathlib import Path
 
 class Dialog(QWidget):
-    def __init__(self, depcache, cache, upgrades, security_updates):
+    def __init__(self, cacheUpdate, fullUpgrade):
         QWidget.__init__(self)
-        uic.loadUi("designer/update_notifier.ui", self)
-        self.upgrades = upgrades
+        #uic.loadUi("designer/update_notifier.ui", self)
+        #self.upgrades = upgrades
         
         self.initUI()
-        self.upgrade_swBtn.clicked.connect(self.call_update_software)
+        #self.upgrade_swBtn.clicked.connect(self.call_update_software)
         self.upgradeBtn.clicked.connect(self.upgrade)
         self.closeBtn.clicked.connect(self.call_reject)
         
     def initUI(self):
-        self.progressBar.setVisible(False)
+        self.label = QLabel()
+        self.label.setAlignment(Qt.AlignHCenter)
+        self.upgradeBtn = QPushButton("Upgrade")
+        self.closeBtn = QPushButton("Close")
+        self.progressBar = QProgressBar()
+        self.treeView = QTreeView()
         
+        hbox=QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.upgradeBtn)
+        hbox.addWidget(self.closeBtn)
+        hbox.addStretch(1)
+        
+        vbox=QVBoxLayout()
+        vbox.addWidget(self.label)
+        vbox.addLayout(hbox)
+        
+        self.setLayout(vbox) 
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Upgrade')
+        
+        
+        self.progressBar.setVisible(False)
+
+'''        
         if self.upgrades > 0:
             self.label.setText("There are %s upgrades available and %s security updates available, do you want to Upgrade, open the Update Software or Close? \n The following are the affected packages:" % (upgrades, security_updates))
             #self.label.setText("There are %s upgrades available and %s security updates available, do you want to Update? \n The following are the affected packages:" % (upgrades, security_updates))
@@ -72,7 +95,7 @@ class Dialog(QWidget):
             self.upgradeBtn.setVisible(False)
             self.upgrade_swBtn.setVisible(False)
             self.setVisible(False)
-            
+'''            
     def upgrade_progress(self, transaction, progress):
         self.upgradeBtn.setVisible(False)
         self.upgrade_swBtn.setVisible(False)
@@ -141,23 +164,7 @@ class Dialog(QWidget):
         model.setHeaderData(0, Qt.Horizontal, "Action")
         model.setHeaderData(1, Qt.Horizontal, "Package")
         return model
-    
-    def call_update_software(self):
-        program = "plasma-discover"
-        arguments = {"--mode", "Update"}
-        process = QProcess()
-        process.setProgram(program)
-        process.setArguments(arguments)
-        process.startDetached()
-        process.start()
-        process.waitForStarted()
-        #app.quit()
-        
-        '''
-        fprogress = apt.progress.base.FetchProgress()
-        iprogress = apt.progress.base.InstallProgress()
-        depcache.commit(fprogress, iprogress)
-        '''
+
     def upgrade(self):
         self.progressBar.setVisible(False)
         self.treeView.setVisible(False)
@@ -187,181 +194,23 @@ class Dialog(QWidget):
         app.quit()
 
 class App(QApplication):
-    def __init__(self, depcache, cache, upgrades, security_updates, *args):
+    def __init__(self, cacheUpdate, fullUpgrade *args):
         QApplication.__init__(self, *args)
-        self.dialog = Dialog(depcache, cache, upgrades, security_updates)
+        self.dialog = Dialog(cacheUpdate, fullUpgrade)
         self.dialog.show()
 
 
-def main(args, depcache, cache, upgrades, security_updates):
+def main(args, cacheUpdate, fullUpgrade):
     global app
-    app = App(depcache, cache, upgrades, security_updates, args)
+    app = App(cacheUpdate, fullUpgrade, args)
     app.setWindowIcon(QIcon.fromTheme("system-software-update"))
     app.exec_()
 
-############################ END QT############################################
-SYNAPTIC_PINFILE = "/var/lib/synaptic/preferences"
-DISTRO = subprocess.check_output(
-    ["lsb_release", "-c", "-s"],
-    universal_newlines=True).strip()
-
-def _(msg):
-    return gettext.dgettext("update-notifier", msg)
-
-
-def _handleException(type, value, tb):
-    sys.stderr.write("E: " + _("Unknown Error: '%s' (%s)") % (type, value))
-    sys.exit(-1)
-
-
-def clean(cache, depcache):
-    " unmark (clean) all changes from the given depcache "
-    # mvo: looping is too inefficient with the new auto-mark code
-    # for pkg in cache.Packages:
-    #     depcache.MarkKeep(pkg)
-    depcache.init()
-
-def saveDistUpgrade(cache, depcache):
-    """ this function mimics a upgrade but will never remove anything, unless clean is commented"""
-    depcache.upgrade(True)
-    '''if depcache.del_count > 0:
-        clean(cache, depcache)
-    depcache.upgrade()'''
-
-def isSecurityUpgrade(ver):
-    " check if the given version is a security update (or masks one) "
-    security_pockets = [("Ubuntu", "%s-security" % DISTRO),
-                        ("gNewSense", "%s-security" % DISTRO),
-                        ("Debian", "%s-updates" % DISTRO)]
-    for (file, index) in ver.file_list:
-        for origin, archive in security_pockets:
-            if (file.archive == archive and file.origin == origin):
-                return True
-    return False
-
-def init():
-    " init the system, be nice "
-    # FIXME: do a ionice here too?
-    os.nice(19)
-    apt_pkg.init()
-
-
-def run():
-
-    # we are run in "are security updates installed automatically?"
-    # question mode
-    '''
-    if options.security_updates_unattended:
-        res = apt_pkg.config.find_i("APT::Periodic::Unattended-Upgrade", 0)
-        # print(res)
-        sys.exit(res)
-     '''
-     '''
-     if options.update_cache
-        apt_client = client.AptClient()
-        trans = apt_client.update_cache(wait=True)
-     '''
-    # get caches
-    try:
-        cache = apt_pkg.Cache(apt.progress.base.OpProgress())
-    except SystemError as e:
-        sys.stderr.write("E: " + _("Error: Opening the cache (%s)") % e)
-        sys.exit(-1)
-    depcache = apt_pkg.DepCache(cache)
-
-    # read the synaptic pins too
-    if os.path.exists(SYNAPTIC_PINFILE):
-        depcache.read_pinfile(SYNAPTIC_PINFILE)
-        depcache.init()
-
-    if depcache.broken_count > 0:
-        sys.stderr.write("E: " + _("Error: BrokenCount > 0"))
-        sys.exit(-1)
-
-    # do the upgrade (not dist-upgrade!)
-    try:
-        saveDistUpgrade(cache, depcache)
-    except SystemError as e:
-        sys.stderr.write("E: " + _("Error: Marking the upgrade (%s)") % e)
-        sys.exit(-1)
-
-    # analyze the ugprade
-    upgrades = 0
-    security_updates = 0
-
-    # we need another cache that has more pkg details
-    with apt.Cache() as aptcache:
-        for pkg in cache.packages:
-            # skip packages that are not marked upgraded/installed
-            if not (depcache.marked_install(pkg)
-                    or depcache.marked_upgrade(pkg)):
-                continue
-            # check if this is really a upgrade or a false positive
-            # (workaround for ubuntu #7907)
-            inst_ver = pkg.current_ver
-            cand_ver = depcache.get_candidate_ver(pkg)
-            if cand_ver == inst_ver:
-                continue
-            # check for security upgrades
-            if isSecurityUpgrade(cand_ver):
-                upgrades += 1
-                security_updates += 1
-                continue
-
-            # check to see if the update is a phased one
-            try:
-                from UpdateManager.Core.UpdateList import UpdateList
-                ul = UpdateList(None)
-                ignored = ul._is_ignored_phased_update(
-                    aptcache[pkg.get_fullname()])
-                if ignored:
-                    depcache.mark_keep(pkg)
-                    continue
-            except ImportError:
-                pass
-
-            upgrades = upgrades + 1
-
-            # now check for security updates that are masked by a
-            # candidate version from another repo (-proposed or -updates)
-            for ver in pkg.version_list:
-                if (inst_ver
-                        and apt_pkg.version_compare(ver.ver_str,
-                                                    inst_ver.ver_str) <= 0):
-                    continue
-                if isSecurityUpgrade(ver):
-                    security_updates += 1
-                    break
-
-        # print the number of regular upgrades and the number of
-        # security upgrades
-        #sys.stderr.write("%s;%s" % (upgrades, security_updates))
-        print("%s;%s" % (upgrades, security_updates))
-
-    # return the number of upgrades (if its used as a module)
-    return(depcache, cache, upgrades, security_updates)
-
-
 
 if __name__ == "__main__":
-    #sys.excepthook = _handleException
-
-    # gettext
-    APP = "update-notifier"
-    DIR = "/usr/share/locale"
-    gettext.bindtextdomain(APP, DIR)
-    gettext.textdomain(APP)
-    
-    global reboot_required_path
-    reboot_required_path = Path("/var/run/reboot-required")
-
-    init()
-    (depcache, cache, upgrades, security_updates) = run()
-        
-    if upgrades > 0:
-        main(sys.argv, depcache, cache, upgrades, security_updates)
-        
-    # check "/var/run/reboot-required" (.pkgs) if exists reboot is needed.
-    if reboot_required_path.exists():
-        main(sys.argv, depcache, cache, 0, 0)
+	#option to update cache or not
+	#option to full-upgrade or safe-upgrade
+	cacheUpdate = False
+	fullUpgrade = False
+	main(sys.argv, cacheUpdate, fullUpgrade)
         
