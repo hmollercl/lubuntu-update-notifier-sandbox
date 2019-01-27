4fcf2e24b6359eb9e595c8714511362e59295e3b
Hans P. Möller <hmoller@gmail.com>
2019-01-27 10:49:19 -0300

    retriving directory of the script

--- upNot.sh
+++ upNot.sh
@@ -2,6 +2,7 @@
 while true;
     do
         #/usr/lib/update-notifier/lubuntu/notifier.py -p ./upgrader.py >> /home/hmoller/update_notifier.log 2>&1
+        cd "$( cd "$( dirname "$0" )" && pwd )"
         ./notifier.py -p ./upgrader.py >> ~/update_notifier.log 2>&1
         date >> ~/update_notifier.log
         sleep 3600
