d1649deb696752105e4edda236339a0e492158f0
Hans P. Möller <hmoller@gmail.com>
2019-02-18 22:37:31 -0300

    bash daemon and upg with shell

--- daemon/notifier_gui.py
+++ daemon/notifier_gui.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3 
 
 import sys
-from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QPushButton, QHBoxLayout, QVBoxLayout)
+from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QPushButton,
+							QHBoxLayout, QVBoxLayout)
 from PyQt5.QtCore import (Qt, QProcess)
 from PyQt5.QtGui import QIcon
 from optparse import OptionParser
@@ -38,7 +39,10 @@ class Dialog(QWidget):
         vbox.addWidget(self.label)
         vbox.addLayout(hbox)
         
-        self.setLayout(vbox) 
+        if self.upg_path == None:
+            self.upgradeBtn.setVisible(False)
+
+        self.setLayout(vbox)
         self.setGeometry(300, 300, 300, 150)
         self.setWindowTitle("Update Notifier")
         
@@ -58,16 +62,25 @@ class Dialog(QWidget):
         app.quit()
     
     def call_upgrade(self):
-        #upg_path= "./upgrader.py"
-
-        process = subprocess.Popen(self.upg_path)
+        self.label.setText("Upgrading....")
+        #TODO maybe open another thread so notifier won't freeze
+        if self.upg_path == "terminal":
+            cmd = ['qterminal', '-e', 'sudo', 'apt', 'dist-upgrade']
+        else:
+            cmd = ['lxqt-sudo', self.upg_path]
+        #process = subprocess.Popen(self.upg_path)
+        #process = subprocess.Popen(cmd)
+        #process = subprocess.Popen(cmd, shell=True)
+        process = subprocess.Popen(cmd)
         process.wait()
         app.quit()
 
 class App(QApplication):
-    def __init__(self, upgrades, security_upgrades, reboot_required, upg_path, *args):
+    def __init__(self, upgrades, security_upgrades, reboot_required, upg_path, 
+    			*args):
         QApplication.__init__(self, *args)
-        self.dialog = Dialog(upgrades, security_upgrades, reboot_required, upg_path)
+        self.dialog = Dialog(upgrades, security_upgrades, reboot_required,
+        					 upg_path)
         self.dialog.show()
 
 def main(args, upgrades, security_upgrades, reboot_required, upg_path):
@@ -89,7 +102,7 @@ if __name__ == "__main__":
                       help="How many upgrades are available",
                       metavar="APP")
     parser.add_option("-s",
-                      "--securrity-upg",
+                      "--security-upg",
                       dest="security_upgrades",
                       help="How many security upgrades are available",
                       metavar="APP")
